06df58f7b066d68cf5d2cb41e0075873a60c89ab
Arvydas Balionis <arvydas@balionis.com>
2024-02-13 21:09:01 +0000

    JIRA-0000 clone lesson5 into lesson6

---
+++ dainius-lesson6/README.md
@@ -0,0 +1,67 @@
+# dainius-lesson5
+
+1. Baseline is dainius-lesson5
+
+2. Add postgres sql.
+
+3. Add spring jpa.
+
+# Build
+```
+./gradlew clean dockerBuild --console=plain
+```
+
+# Run (Start under docker)
+```
+./gradlew dockerComposeUp --console=plain
+```
+
+# Test
+
+## Tail Docker Logs
+
+```
+docker container ls
+CONTAINER ID   IMAGE                    COMMAND                  CREATED          STATUS          PORTS                    NAMES
+99a13e4e3ac0   dainius-lesson6:latest   "/bin/sh -c 'java $J…"   21 seconds ago   Up 20 seconds   0.0.0.0:8080->9090/tcp   dainius-lesson5-service1-1
+
+arvydas@bamac01 ~ % docker logs -f 99a13e4e3ac0                                     
+16:36:13.516 [main] INFO  c.b.dainius.lesson6.Application - starting
+16:36:14.388 [main] WARN  o.s.b.l.logback.LogbackLoggingSystem - Ignoring 'logback.configurationFile' system property. Please use 'logging.config' instead.
+
+  .   ____          _            __ _ _
+ /\\ / ___'_ __ _ _(_)_ __  __ _ \ \ \ \
+( ( )\___ | '_ | '_| | '_ \/ _` | \ \ \ \
+ \\/  ___)| |_)| | | | | || (_| |  ) ) ) )
+  '  |____| .__|_| |_|_| |_\__, | / / / /
+ =========|_|==============|___/=/_/_/_/
+ :: Spring Boot ::                (v3.2.2)
+
+16:36:14.557 [main] INFO  c.b.dainius.lesson6.Application - Starting Application using Java 17-ea with PID 1 (/app/app.jar started by root in /app)
+16:36:14.558 [main] INFO  c.b.dainius.lesson6.Application - The following 1 profile is active: "docker"
+16:36:16.591 [main] INFO  o.s.b.w.e.tomcat.TomcatWebServer - Tomcat initialized with port 9090 (http)
+...
+16:37:28.966 [http-nio-9090-exec-1] INFO  o.s.web.servlet.DispatcherServlet - Completed initialization in 1 ms
+16:37:28.997 [http-nio-9090-exec-1] INFO  c.b.d.l.rest.HeartbeatController - alive...
+```
+
+## Call service under docker
+```
+curl -X GET http://localhost:9090/api/v1/heartbeat
+...
+curl -X POST http://localhost:9090/api/v1/moving --header 'Content-Type: application/json' --data '{ "number": "2.0"}'
+curl -X GET http://localhost:9090/api/v1/moving
+
+// see docker container logs (see: 'Tail Docker Logs' above for how-to)
+... 
+arvydas@bamac01 ~ % docker logs -f 99a13e4e3ac0                                     
+...
+16:36:17.383 [main] INFO  c.b.dainius.lesson5.Application - finishing
+16:37:28.965 [http-nio-9090-exec-1] INFO  o.a.c.c.C.[Tomcat].[localhost].[/] - Initializing Spring DispatcherServlet 'dispatcherServlet'
+16:37:28.965 [http-nio-9090-exec-1] INFO  o.s.web.servlet.DispatcherServlet - Initializing Servlet 'dispatcherServlet'
+16:37:28.966 [http-nio-9090-exec-1] INFO  o.s.web.servlet.DispatcherServlet - Completed initialization in 1 ms
+16:37:28.997 [http-nio-9090-exec-1] INFO  c.b.d.l.rest.HeartbeatController - alive...
+...
+```
+
+
---
+++ dainius-lesson6/build.gradle.kts
@@ -0,0 +1,167 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("java")
+    id("jacoco")
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.openapi.generator") version "7.2.0"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+dependencies {
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.slf4j:slf4j-api:2.0.11")
+    runtimeOnly("ch.qos.logback:logback-core:1.4.14")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.14")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+
+    testImplementation("org.mockito:mockito-core:5.3.1")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    compileOnly("javax.servlet:javax.servlet-api:4.0.1")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.20")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+openApiValidate {
+    inputSpec.set("$rootDir/specs/dainius-lesson6-api.yaml")
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$rootDir/specs/dainius-lesson6-api.yaml")
+    outputDir.set("$buildDir/generated")
+    apiPackage.set("com.balionis.dainius.lesson6.generated.api")
+    modelPackage.set("com.balionis.dainius.lesson6.generated.model")
+    configOptions.set(mapOf(
+        "dateLibrary" to "java8",
+        "generateApis" to "true",
+        "generateApiTests" to "false",
+        "generateModels" to "true",
+        "generateModelTests" to "false",
+        "generateModelDocumentation" to "false",
+        "generateSupportingFiles" to "false",
+        "hideGenerationTimestamp" to "true",
+        "interfaceOnly" to "true",
+        "library" to "spring-boot",
+        "serializableModel" to "true",
+        "useBeanValidation" to "true",
+        "useTags" to "true",
+        "implicitHeaders" to "true",
+        "openApiNullable" to "false",
+        "oas3" to "true"
+    ))
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs(listOf("${buildDir}/generated/src/main/java"))
+        }
+    }
+}
+
+tasks.compileJava {
+    dependsOn(tasks.openApiValidate, tasks.openApiGenerate)
+}
+
+jacoco {
+    toolVersion = "0.8.9"
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    maxHeapSize = "1G"
+
+    testLogging {
+        showStandardStreams = true
+        events("passed")
+    }
+
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "CLASS"
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.90".toBigDecimal()
+            }
+            excludes = listOf(
+                "com.balionis.dainius.lesson6.Application",
+                "com.balionis.dainius.lesson6.configuration.*",
+                "com.balionis.dainius.lesson6.generated.*"
+            )
+        }
+    }
+    dependsOn(tasks.test)
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = false
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacoco-html")
+    }
+
+    dependsOn(tasks.test)
+}
+
+tasks.named<BootJar>("bootJar") {
+    mainClass.set("com.balionis.dainius.lesson6.Application")
+    archiveFileName.set("${archiveBaseName.get()}-all.${archiveExtension.get()}");
+}
+
+tasks {
+
+    create<Exec>("dockerBuild") {
+        dependsOn("bootJar")
+
+        description = "Build a service docker image"
+        group = "Docker"
+        commandLine("docker","build","-t","${this.project.name}:latest",".")
+    }
+
+    create<Exec>("dockerComposeUp") {
+        description = "Start a service on local docker"
+        group = "Docker"
+        commandLine("docker-compose","-f","docker-compose.yml","build")
+        commandLine("docker-compose","-f","docker-compose.yml","up","-d")
+    }
+
+    create<Exec>("dockerComposeDown") {
+        description = "Stop a service on local docker"
+        group = "Docker"
+        commandLine("docker-compose","-f","docker-compose.yml","down")
+    }
+}
